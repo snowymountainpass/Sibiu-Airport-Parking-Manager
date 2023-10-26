3b8ea4d2eec623a23f2132b44ca353b7d8f41d5a
snowymountainpass <73165724+snowymountainpass@users.noreply.github.com>
2023-10-26 13:07:03 +0200

    Initial commit

---
+++ src/main/java/com/clockworkcode/sibiuairportparkingmanager/SibiuAirportParkingManagerApplication.java
@@ -0,0 +1,13 @@
+package com.clockworkcode.sibiuairportparkingmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SibiuAirportParkingManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SibiuAirportParkingManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clockworkcode/sibiuairportparkingmanager/SibiuAirportParkingManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clockworkcode.sibiuairportparkingmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SibiuAirportParkingManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
