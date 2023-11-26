907f5f958a312ac60171e310723974a06ec7b0ad
snowymountainpass <73165724+snowymountainpass@users.noreply.github.com>
2023-11-26 12:24:34 +0100

    Added payment page and functionality

---
+++ frontend/sibiu-airport/src/Components/PaymentForm.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+
+const PaymentForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+    const history = useHistory();
+
+    const [amount, setAmount] = useState(0);
+
+    const handleAmountChange = (event) => {
+        setAmount(parseFloat(event.target.value));
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if (!stripe || !elements) {
+            return;
+        }
+
+        const { token, error } = await stripe.createToken(elements.getElement(CardElement), { amount });
+
+        if (error) {
+            console.error(error);
+        } else {
+            console.log('Token:', token);
+            // Assuming your payment processing logic returns a success status
+            const paymentSuccess = true; // Replace with your actual success check
+
+            if (paymentSuccess) {
+                // Redirect to the Confirmation Page
+                history.push('/confirmation');
+            } else {
+                console.log('Payment failed');
+                // Handle payment failure if needed
+            }
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Amount (in cents):
+                <input type="number" value={amount} onChange={handleAmountChange} />
+            </label>
+            <CardElement />
+            <button type="submit" disabled={!stripe}>
+                Pay
+            </button>
+        </form>
+    );
+};
+
+export default PaymentForm;
---
+++ frontend/sibiu-airport/src/Components/PaymentPageBody.js
@@ -0,0 +1,46 @@
+// PaymentPageBody.js
+import React from 'react';
+import { Divider, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
+import PaymentForm from './PaymentForm';
+
+const PaymentPageBody = ({ paymentDetails }) => {
+    const carLicenseNumber = localStorage.getItem('carLicenseNumber');
+    const startDate = '2023-11-20T08:00:00'; // TODO: replace with server stored start date & time
+
+    // TODO: replace with current server date & time
+    const endDate = new Date().toISOString();
+
+    return (
+        <div>
+            <PaymentForm />
+
+            <Divider style={{ margin: '20px 0' }} />
+
+            <Typography variant="h6" gutterBottom>
+                Payment Details Table
+            </Typography>
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Car License Number</TableCell>
+                            <TableCell>Start Date & Time</TableCell>
+                            <TableCell>End Date & Time</TableCell>
+                            <TableCell>Payment Amount</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>{carLicenseNumber}</TableCell>
+                            <TableCell>{startDate}</TableCell>
+                            <TableCell>{endDate}</TableCell>
+                            <TableCell>{paymentDetails.amount / 100} EUR</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
+    );
+};
+
+export default PaymentPageBody;
