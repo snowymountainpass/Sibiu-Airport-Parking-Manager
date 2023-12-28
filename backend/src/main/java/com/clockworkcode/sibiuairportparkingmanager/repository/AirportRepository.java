c00cb50a96c7fce058102b0a8ba4f13271e76beb
snowymountainpass <73165724+snowymountainpass@users.noreply.github.com>
2023-12-28 17:20:59 +0100

    Implemented Airport Repository interface
    & updated Airport class

--- backend/src/main/java/com/clockworkcode/sibiuairportparkingmanager/repository/AirportRepository.java
+++ backend/src/main/java/com/clockworkcode/sibiuairportparkingmanager/repository/AirportRepository.java
@@ -3,5 +3,14 @@ package com.clockworkcode.sibiuairportparkingmanager.repository;
 import com.clockworkcode.sibiuairportparkingmanager.model.Airport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AirportRepository extends JpaRepository<Airport,Integer> {
+import java.util.List;
+
+public interface AirportRepository extends JpaRepository<Airport,Long> {
+
+    Airport findAirportByAirportId(Long airportId);
+    Airport findAirportByAirportIBAN(String airportIban);
+    Airport findAirportByAirportLocation (String airportLocation);
+
+
+
 }
