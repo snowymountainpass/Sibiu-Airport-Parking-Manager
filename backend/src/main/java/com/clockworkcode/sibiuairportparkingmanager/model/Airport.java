08d1585706ea77ce9d18c0ea81f470b951f4402c
snowymountainpass <73165724+snowymountainpass@users.noreply.github.com>
2023-12-28 11:31:13 +0100

    implemented Airport entity

--- backend/src/main/java/com/clockworkcode/sibiuairportparkingmanager/model/Airport.java
+++ backend/src/main/java/com/clockworkcode/sibiuairportparkingmanager/model/Airport.java
@@ -1,4 +1,49 @@
 package com.clockworkcode.sibiuairportparkingmanager.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Airport {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long airportId;
+    private String location;
+    private String airportIBAN;
+
+    public Airport(Long airportId, String location, String airportIBAN) {
+        this.airportId = airportId;
+        this.location = location;
+        this.airportIBAN = airportIBAN;
+    }
+
+    public Airport() {
+        
+    }
+
+    public Long getAirportId() {
+        return airportId;
+    }
+
+    public void setAirportId(Long airportId) {
+        this.airportId = airportId;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getAirportIBAN() {
+        return airportIBAN;
+    }
+
+    public void setAirportIBAN(String airportIBAN) {
+        this.airportIBAN = airportIBAN;
+    }
 }
