fed78a35113a745322824096d9448860d4eedb92
snowymountainpass <73165724+snowymountainpass@users.noreply.github.com>
2023-12-28 12:27:56 +0100

    Implemented remaining entity classes
    updated Airport class

--- backend/src/main/java/com/clockworkcode/sibiuairportparkingmanager/model/ParkingSpace.java
+++ backend/src/main/java/com/clockworkcode/sibiuairportparkingmanager/model/ParkingSpace.java
@@ -1,4 +1,51 @@
 package com.clockworkcode.sibiuairportparkingmanager.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class ParkingSpace {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long parkingSpaceId;
+
+    private String parkingNumber;
+
+    private boolean availability;
+
+    public ParkingSpace(String parkingNumber, boolean availability) {
+        this.parkingNumber = parkingNumber;
+        this.availability = availability;
+    }
+
+    public ParkingSpace() {
+    }
+
+    public Long getParkingSpaceId() {
+        return parkingSpaceId;
+    }
+
+    public void setParkingSpaceId(Long parkingSpaceId) {
+        this.parkingSpaceId = parkingSpaceId;
+    }
+
+    public String getParkingNumber() {
+        return parkingNumber;
+    }
+
+    public void setParkingNumber(String parkingNumber) {
+        this.parkingNumber = parkingNumber;
+    }
+
+    public boolean isAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
+
 }
--- backend/src/main/java/com/clockworkcode/sibiuairportparkingmanager/model/Payment.java
+++ backend/src/main/java/com/clockworkcode/sibiuairportparkingmanager/model/Payment.java
@@ -1,4 +1,78 @@
 package com.clockworkcode.sibiuairportparkingmanager.model;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+@Entity
 public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long paymentId;
+
+    @ManyToOne
+    @JoinColumn(name = "parking_space_id")
+    private ParkingSpace parkingSpace;
+
+    private Timestamp startTime;
+    private Timestamp endTime;
+    private BigDecimal cost;
+    private Timestamp paymentDateTime;
+
+    public Payment(ParkingSpace parkingSpace, Timestamp startTime, Timestamp endTime, BigDecimal cost, Timestamp paymentDateTime) {
+        this.parkingSpace = parkingSpace;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.cost = cost;
+        this.paymentDateTime = paymentDateTime;
+    }
+
+    public Payment() {
+    }
+
+    public Long getPaymentId() {
+        return paymentId;
+    }
+
+    public ParkingSpace getParkingSpace() {
+        return parkingSpace;
+    }
+
+    public void setParkingSpace(ParkingSpace parkingSpace) {
+        this.parkingSpace = parkingSpace;
+    }
+
+    public Timestamp getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Timestamp startTime) {
+        this.startTime = startTime;
+    }
+
+    public Timestamp getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Timestamp endTime) {
+        this.endTime = endTime;
+    }
+
+    public BigDecimal getCost() {
+        return cost;
+    }
+
+    public void setCost(BigDecimal cost) {
+        this.cost = cost;
+    }
+
+    public Timestamp getPaymentDateTime() {
+        return paymentDateTime;
+    }
+
+    public void setPaymentDateTime(Timestamp paymentDateTime) {
+        this.paymentDateTime = paymentDateTime;
+    }
 }
