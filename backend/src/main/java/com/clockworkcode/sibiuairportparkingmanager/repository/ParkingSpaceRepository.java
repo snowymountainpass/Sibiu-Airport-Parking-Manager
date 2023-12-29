5605671b09a1858fbc1a2d0fb24dc0bef83a13e8
snowymountainpass <73165724+snowymountainpass@users.noreply.github.com>
2023-12-29 15:29:37 +0100

    Started working on airport and parking space repositories
    & tested saving airports and parking spaces into the DB in order to observe the connection between the two entities

--- backend/src/main/java/com/clockworkcode/sibiuairportparkingmanager/repository/ParkingSpaceRepository.java
+++ backend/src/main/java/com/clockworkcode/sibiuairportparkingmanager/repository/ParkingSpaceRepository.java
@@ -3,5 +3,11 @@ package com.clockworkcode.sibiuairportparkingmanager.repository;
 import com.clockworkcode.sibiuairportparkingmanager.model.ParkingSpace;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ParkingSpaceRepository extends JpaRepository<ParkingSpace,Integer> {
+import java.util.List;
+
+public interface ParkingSpaceRepository extends JpaRepository<ParkingSpace,Long> {
+
+    List<ParkingSpace>  findParkingSpacesByParkingSpaceAvailable(Boolean parkingSpaceAvailable);
+    ParkingSpace findParkingSpaceByParkingNumber(String parkingNumber);
+
 }
