56069210ab08806d40d7069e2760d93a95d2d878
snowymountainpass <73165724+snowymountainpass@users.noreply.github.com>
2024-02-10 17:59:57 +0100

    Started implementation of ParkingSpaceController class

--- backend/src/main/java/com/clockworkcode/sibiuairportparkingmanager/controller/ParkingSpaceController.java
+++ backend/src/main/java/com/clockworkcode/sibiuairportparkingmanager/controller/ParkingSpaceController.java
@@ -1,4 +1,30 @@
 package com.clockworkcode.sibiuairportparkingmanager.controller;
 
+import com.clockworkcode.sibiuairportparkingmanager.model.ParkingSpace;
+import com.clockworkcode.sibiuairportparkingmanager.service.ParkingActivityService;
+import com.clockworkcode.sibiuairportparkingmanager.service.ParkingSpaceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/parkingSpaces")
 public class ParkingSpaceController {
+
+    @Autowired
+    private ParkingSpaceService parkingSpaceService;
+    private ParkingActivityService parkingActivityService;
+
+    @GetMapping("/getParkingSpace/{licensePlate}")
+    public ResponseEntity<ParkingSpace> getParkingSpace(@PathVariable String licensePlate){
+        return null;
+    }
+
+
+
+
 }
---
+++ backend/src/main/java/com/clockworkcode/sibiuairportparkingmanager/model/ParkingCostController.java
@@ -0,0 +1,7 @@
+package com.clockworkcode.sibiuairportparkingmanager.model;
+
+public class ParkingCostController {
+
+    //TODO: 9) Determine which Controller should handle the main Parking Space Cost for a Car - P.Cost or P.Activity
+
+}
